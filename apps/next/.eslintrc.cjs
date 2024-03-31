809182c52bb60d35b38c189e5e63d54ac7384d2c
vetan2 <vetan2@naver.com>
2024-03-31 19:45:08 +0900

    feat: Update package dependencies and paths
    - Update paths in tsconfig.json to include new directory structure
    - Update next version to 14.1.4 in package.json
    - Update devDependencies in package.json
    - Remove .eslintrc.json
    - Add .prettierrc.json configuration
    - Update .eslintrc.cjs configuration
    - Refactor styles in app/page.tsx
    - Update dependencies and rules in .eslintrc.cjs

---
+++ apps/next/.eslintrc.cjs
@@ -0,0 +1,70 @@
+/**
+ * @type {import("eslint").Linter.Config}
+ */
+module.exports = {
+  extends: ["next/core-web-vitals", "@vetan2/react"],
+  plugins: ["@vetan2"],
+  rules: {
+    "@vetan2/import-path": 1,
+
+    "import/order": [
+      1,
+      {
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+        "newlines-between": "always",
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          ["parent", "sibling", "index"],
+        ],
+        pathGroups: [
+          {
+            pattern: "~*/**",
+            group: "internal",
+          },
+          {
+            pattern: "@*/**",
+            group: "internal",
+          },
+        ],
+      },
+    ],
+    "import/no-extraneous-dependencies": [
+      1,
+      {
+        devDependencies: [
+          ".storybook/**",
+          "**/*.config.*",
+          "scripts/**",
+          "**/*.stories.ts*",
+        ],
+      },
+    ],
+    "prettier/prettier": [1, {}, { usePrettierrc: true }],
+  },
+  overrides: [
+    {
+      files: ["layout.tsx", "page.tsx"],
+      rules: {
+        "react/function-component-definition": [
+          2,
+          { namedComponents: "function-declaration" },
+        ],
+      },
+    },
+    {
+      files: ["**/*.test.ts*"],
+      env: {
+        jest: true,
+      },
+    },
+    {
+      files: ["**/*.config.*"],
+      rules: { "import/no-anonymous-default-export": 0 },
+    },
+  ],
+};
