a29cee39afa5488cd568ba1751a42a7c455d4c42
vetan2 <vetan2@naver.com>
2024-03-31 22:43:28 +0900

    refactor: Consolidate content and extend configuration in TailwindCSS
    - Consolidated content paths into a single array for easier readability
    - Updated the conic gradient background style in the theme extension block

--- apps/next/.eslintrc.cjs
+++ apps/next/.eslintrc.cjs
@@ -2,11 +2,7 @@
  * @type {import("eslint").Linter.Config}
  */
 module.exports = {
-  extends: [
-    "next/core-web-vitals",
-    "@vetan2/react",
-    "plugin:storybook/recommended",
-  ],
+  extends: ["next/core-web-vitals", "plugin:@vetan2/react", "plugin:storybook/recommended"],
   plugins: ["@vetan2"],
   rules: {
     "@vetan2/import-path": 1,
@@ -19,12 +15,7 @@ module.exports = {
           caseInsensitive: true,
         },
         "newlines-between": "always",
-        groups: [
-          "builtin",
-          "external",
-          "internal",
-          ["parent", "sibling", "index"],
-        ],
+        groups: ["builtin", "external", "internal", ["parent", "sibling", "index"]],
         pathGroups: [
           {
             pattern: "~*/**",
@@ -40,12 +31,7 @@ module.exports = {
     "import/no-extraneous-dependencies": [
       1,
       {
-        devDependencies: [
-          ".storybook/**",
-          "**/*.config.*",
-          "scripts/**",
-          "**/*.stories.ts*",
-        ],
+        devDependencies: [".storybook/**", "**/*.config.*", "scripts/**", "**/*.stories.ts*"],
       },
     ],
     "prettier/prettier": [1, {}, { usePrettierrc: true }],
@@ -54,10 +40,7 @@ module.exports = {
     {
       files: ["layout.tsx", "page.tsx"],
       rules: {
-        "react/function-component-definition": [
-          2,
-          { namedComponents: "function-declaration" },
-        ],
+        "react/function-component-definition": [2, { namedComponents: "function-declaration" }],
       },
     },
     {
--- apps/next/tailwind.config.ts
+++ apps/next/tailwind.config.ts
@@ -1,16 +1,12 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
