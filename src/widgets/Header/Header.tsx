0b45618d585b5d57b2352de1749d0fa7cc642293
-g <vetan2@naver.com>
2024-05-30 22:33:12 +0900

    feat: Logo에 Link 달기

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,6 +1,7 @@
-import { Spacer } from "@nextui-org/react";
+import { Link, Spacer } from "@nextui-org/react";
+import NextLink from "next/link";
 
-import Logo from "~/shared/assets/images/logo.svg";
+import { Logo } from "~/shared/assets/images";
 
 import { MenuButton } from "./MenuButton";
 
@@ -9,7 +10,9 @@ export const Header = () => {
     <header className="flex items-center border-b-1 border-b-default-100 p-4">
       <MenuButton />
       <Spacer x={2} />
-      <Logo className="h-6 md:h-8" />
+      <Link as={NextLink} href="/">
+        <Logo className="h-6 md:h-8" />
+      </Link>
     </header>
   );
 };
