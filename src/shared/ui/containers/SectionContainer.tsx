5c7caee189d4d46065394ef5ef666fad81bb6f92
-g <vetan2@naver.com>
2024-06-10 16:15:13 +0900

    feat: SettingContainer 보완

--- src/shared/ui/containers/SectionContainer.tsx
+++ src/shared/ui/containers/SectionContainer.tsx
@@ -1,15 +1,23 @@
-import { type HTMLAttributes } from "react";
+import { type ReactNode, forwardRef, type HTMLAttributes } from "react";
 
 import { cx } from "~/shared/style";
 
-export const SectionContainer = ({
-  className,
-  ...props
-}: HTMLAttributes<HTMLDivElement>) => {
+export const SectionContainer = forwardRef<
+  HTMLDivElement,
+  HTMLAttributes<HTMLDivElement> & {
+    title?: ReactNode;
+  }
+>(({ className, title, children, ...props }, ref) => {
   return (
-    <div
+    <section
       className={cx("rounded-3xl bg-white p-4 shadow-section", className)}
+      ref={ref}
       {...props}
-    />
+    >
+      {title && (
+        <h2 className="text-base font-bold text-primary lg:text-lg">{title}</h2>
+      )}
+      {children}
+    </section>
   );
-};
+});
