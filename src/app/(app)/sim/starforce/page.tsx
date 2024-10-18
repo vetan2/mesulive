a2a370e1841437e4cd5fe7cf39ce9e707387490f
-g <vetan2@naver.com>
2024-10-18 18:31:46 +0900

    feat: metadata 추가

--- src/app/(app)/sim/starforce/page.tsx
+++ src/app/(app)/sim/starforce/page.tsx
@@ -1,3 +1,7 @@
+import { redirect } from "next/navigation";
+
 export default function Page() {
-  return <div>스타포스 시뮬레이터</div>;
+  redirect("https://legacy.mesu.live/sim/starforce");
+
+  return <></>;
 }
